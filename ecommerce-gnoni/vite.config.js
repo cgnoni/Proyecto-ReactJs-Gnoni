e5446d99068ad8969fa9a9efb142ba0798e4a818
cgnoni <c.gnoni1@gmail.com>
2023-05-07 16:20:53 -0300

    Deploy 2

--- ecommerce-gnoni/vite.config.js
+++ ecommerce-gnoni/vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Stakepool-Frontend/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "node_modules"),
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
